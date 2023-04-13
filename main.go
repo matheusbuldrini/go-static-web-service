7d5e951d0a7a4c29cb480806c5e0c3ab3f7699eb
Matheus Buldrini <38816666+matheusbuldrini@users.noreply.github.com>
2023-04-12 22:38:43 -0300

    Using query parameters

--- main.go
+++ main.go
@@ -7,7 +7,15 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello!! :) "))
+
+		names := r.URL.Query()["name"]
+
+		var name string
+		if len(names) == 1 {
+			name = names[0]
+		}
+
+		w.Write([]byte("Hello " + name + " :) "))
 	})
 
 	err := http.ListenAndServe(":3000", nil)
